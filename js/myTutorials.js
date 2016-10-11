2558d9d3f84ccd98573a76c8d33d398d2c152261
Hillyryan <hillyryan@gmail.com>
2016-10-11 12:33:14 +0800

    Add files via upload

---
+++ js/myTutorials.js
@@ -0,0 +1,14 @@
+myFunction([
+{
+"display": "Matchweek 1",
+"url": "Liverpool v Chelsea"
+},
+{
+"display": "HTML Tutorial",
+"url": "http://www.w3schools.com/html/default.asp"
+},
+{
+"display": "CSS Tutorial",
+"url": "http://www.w3schools.com/css/default.asp"
+}
+]);
